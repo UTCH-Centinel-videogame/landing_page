cff3ef7c34993d1e1c6bc86864bbfa3b1d2e635b
Brayan Prieto Lozoya <brayan8prieto@hotmail.com>
2023-03-24 02:03:18 -0700

    landing base done

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -8,5 +8,20 @@ import { Component } from '@angular/core';
 export class HomePage {
 
   constructor() {}
+  isHovering = false;
 
+  handleMouseMove(event: any): void {
+    const circle = document.querySelector('.circle') as HTMLElement;
+    const rect = event.target.getBoundingClientRect();
+    const x = event.clientX - rect.left;
+    const y = event.clientY - rect.top;
+    const halfWidth = circle.offsetWidth / 2;
+    const halfHeight = circle.offsetHeight / 2;
+    circle.style.transform = `translate(${x - halfWidth}px, ${y - halfHeight}px)`;
+    this.isHovering = true;
+  }
+
+  handleMouseLeave(): void {
+    this.isHovering = false;
+  }
 }
